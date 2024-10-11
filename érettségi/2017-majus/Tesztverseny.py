30b4445e214aa1d27caad4462f1dc88db4ea7159
PlumHead69 <lenardf.horvath@gmail.com>
2024-10-12 00:27:33 +0200

    Finished

--- érettségi/2017-majus/Tesztverseny.py
+++ érettségi/2017-majus/Tesztverseny.py
@@ -33,28 +33,31 @@ def Pontszamlalo(valaszok, jovalaszok):
 
 def Dobogosok(pontos):
     
-    dijasok = []
-    dijak = 3
-    kiadottdijak = 0
+    dobogosok = []
 
     for i in range(len(pontos)):
+        helyezet = []
+        if len(dobogosok) < 1:
+            helyezet = [pontos[i], 1]
+            dobogosok.append(helyezet)
+            continue
 
-        if len(dijasok) == 0:
-            dijasok.append(pontos[i])
-            dijak-=1
-        elif dijasok[i-1][1] == pontos[i][1] and dijasok[i-1][0] != pontos[i][0]:
-            print("fasz")
-            dijasok.append(pontos)
-            dijak+=1
-            kiadottdijak+=1
+        if dobogosok[i-1][0][1] == pontos[i][1]:
+            helyezet = [pontos[i], dobogosok[i-1][1]]
+            dobogosok.append(helyezet)
         else:
-            dijasok.append(pontos[i])
-            kiadottdijak+=1
+            helyezet = [pontos[i], dobogosok[i-1][1]+1]
+            dobogosok.append(helyezet)
+            
+        if dobogosok[-1][1] > 3:
+            dobogosok.pop(-1)
+            break
 
-       
+    return dobogosok
+        
         
 
-    return dijasok[0:dijak]
+    
 
 f = open("valaszok.txt","r")
 
@@ -75,8 +78,8 @@ print(f"4.Feladat: \n{jovalaszok}\t(a helyes megoldás)")
 print(f"{Helyes(jovalaszok,versenyzok[azonkeres])}\t(a versenyző helyes válaszai)\n")
 sorszam = int(input("5. feladat: A feladat sorszáma = ") or "10")
 helyesvalasz = Jovalaszokdb(sorszam,versenyzok,jovalaszok)
-print(f"A feladatra {helyesvalasz} fő, a versenyzők {round(helyesvalasz/len(versenyzok) * 100,2)}%-a adott helyes választ.")
-
+print(f"A feladatra {helyesvalasz} fő, a versenyzők {round(helyesvalasz/len(versenyzok) * 100,2)}%-a adott helyes választ.\n")
+print("6. feladat: A versenyzők pontszámának meghatározása\n")
 pontos = []
 
 for kulcs, ertek in versenyzok.items():
@@ -96,8 +99,8 @@ def Rendezes(kl):
 
 pontos.sort(key = Rendezes, reverse = True)
 
-print(Dobogosok(pontos))
-
-
-
+print("7. feladat: A verseny legjobbjai:")
+dobogos = Dobogosok(pontos)
+for i in range(len(dobogos)):
+    print(f"{dobogos[i][1]}.díj ({dobogos[i][0][1]} pont) : {dobogos[i][0][0]}")
 
