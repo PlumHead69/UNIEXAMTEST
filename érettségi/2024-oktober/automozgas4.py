76c56d6aa42a6c86b10deaf39f7b74985c7ad53a
PlumHead69 <lenardf.horvath@gmail.com>
2025-04-29 17:13:09 +0200

    finished

--- érettségi/2024-oktober/automozgas4.py
+++ érettségi/2024-oktober/automozgas4.py
@@ -31,25 +31,41 @@ print(f"\n5. feladat\nA legnagyobb sebesség km/h: {MAX_SPEED}\nA járművek: {'
 #6
 
 def time_calc(first, second):
-    if first[0]<second[0]:
-        return (second[0]*60+second[1])-(first[1]*60+first[0])
-    else:
+    if first[0]==second[0]:
         return second[1]-first[1]
-
-
+    else:
+        return second[1]+60-first[1]
 
 
 print(f"\n6. feladat")
 RENDSZAM= input("Kerem adja meg a rendszamot:") or "ZVJ-638"
 
-valasztott_kocsi = [signal for signal in signals if signal[0]==RENDSZAM]
+chosen_car = [signal for signal in signals if signal[0]==RENDSZAM]
 
-cur=0.0
+s=0.0
+for i in range(len(chosen_car)):
+    if i==0:
+        print(str(chosen_car[i][1])+":"+str(chosen_car[i][2]) +" "+ str(s) +"km")
+    else:
+        s+= round(chosen_car[i-1][3]*(time_calc((chosen_car[i-1][1],chosen_car[i-1][2]),(chosen_car[i][1],chosen_car[i][2]))/60),1)
+        print(str(chosen_car[i][1])+":"+str(chosen_car[i][2]) +" "+ str(s) + "km")
 
-for i in range(len(valasztott_kocsi)):
-    if i == 0:
-        cur += time_calc()/60/valasztott_kocsi[i]
 
+#7
+
+k=open("ido.txt","w")
+
+rendszamok = {}
+for signal in signals:
+    if signal[0] not in rendszamok:
+        rendszamok[signal[0]] = (str(signal[1])+" "+str(signal[2]),"0")
+    else:
+        rendszamok[signal[0]] = (rendszamok[signal[0]][0],str(signal[1])+" "+str(signal[2]))
+
+for value,key in rendszamok.items():
+    k.write(value +" "+key[0] +" "+ key[1] + "\n")
+
+k.close()
 
 
 
